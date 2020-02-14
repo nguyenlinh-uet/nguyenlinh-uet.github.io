34e5c3faa1d9270d528a5e638173319390b9c6a4
nguyenlinh-uet <kenny.nguyen.mat@gmail.com>
2020-02-14 22:42:37 +0700

    Initial commit

---
+++ js/ailurophilia.js
@@ -0,0 +1,1 @@
+No Content: https://charisseysabel.github.io/words/js/ailurophilia.js
---
+++ js/diy.js
@@ -0,0 +1,7 @@
+$(function(){
+    $('#dowebok').fullpage({
+        'navigation': false,
+        'navigationPosition':"left",
+        'navigationColor':['#fff'],
+    });
+});
